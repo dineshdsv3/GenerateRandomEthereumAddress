3eb07dbdaf72be4295b5c84fceb0a81b94f9afd9
dineshdsv3 <43554866+dineshdsv3@users.noreply.github.com>
2019-06-07 08:39:37 +0530

    Add files via upload

---
+++ RandomAddress-0.4.19.sol.sol
@@ -0,0 +1,21 @@
+﻿pragma solidity 0.4.19;
+
+
+    /**
+    * @dev Generate a unique ID that looks like an Ethereum address
+    *
+    * Sample: 0xf4a8f74879182ff2a07468508bec89e1e7464027		          
+    */  
+  contract ethereumaddr{
+    function getUniqueId() public view returns (address) 
+    {
+
+        bytes20 b = bytes20(keccak256(msg.sender, now));
+        uint addr = 0;
+        for (uint index = b.length-1; index+1 > 0; index--) {
+            addr += uint(b[index]) * ( 16 ** ((b.length - index - 1) * 2));
+        }
+
+        return address(addr);
+    }
+}
